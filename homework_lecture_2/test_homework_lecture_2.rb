16e27479cfaadbe67f93258ffa97d4589adaf14e
sergio <khotsenkos1993k@gmail.com>
2024-01-18 14:55:31 +0300

    Add method foobar and test for it

--- homework_lecture_2/test_homework_lecture_2.rb
+++ homework_lecture_2/test_homework_lecture_2.rb
@@ -12,5 +12,14 @@ class StringExtensionTest < Test::Unit::TestCase
     assert_equal(greeting(1), "Неправильный ввод. Напишите, пожалуйста, свое имя, фамилию и возраст.")
     assert_equal(greeting(man_without_age), "Неправильный ввод. Напишите, пожалуйста, свое имя, фамилию и возраст.")
   end
+
+  def test_assert_two_numbers
+    assert_equal(foobar(1, 20), 1)
+    assert_equal(foobar(20, 3), 3)
+    assert_equal(foobar(1, 14), 15)
+    assert_raise ArgumentError do
+      foobar('Man')
+    end
+  end
 end
 
