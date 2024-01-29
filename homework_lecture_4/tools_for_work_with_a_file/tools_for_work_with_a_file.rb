1c4c6ba03ae7a358051cec51f7ca63895645fd5c
sergio <khotsenkos1993k@gmail.com>
2024-01-29 17:05:38 +0300

    Add methods to class

--- homework_lecture_4/tools_for_work_with_a_file/tools_for_work_with_a_file.rb
+++ homework_lecture_4/tools_for_work_with_a_file/tools_for_work_with_a_file.rb
@@ -1,55 +1,60 @@
-WRITERS_LIST_PATH = 'homework_lecture_4/tools_for_work_with_a_file/writers.txt'
-BUFFER = 'buffer.txt'
-def index
-  File.foreach(WRITERS_LIST_PATH) {|writer| puts writer}
-end
+class CustomFile
+  def initialize(path)
+    @file = path
+    @buffer = 'buffer.txt'
+  end
 
-def find(id)
-  file = File.open(WRITERS_LIST_PATH)
-  puts file.readlines.map(&:chomp)[id-1]
-  file.close
-end
+  def index
+    File.foreach(@file) {|writer| puts writer}
+  end
 
-def where(pattern)
-  file = File.open(BUFFER, 'w')
-  list_of_indicies = []
+  def find(id)
+    file = File.open(@file)
+    puts file.readlines.map(&:chomp)[id-1]
+    file.close
+  end
 
-  File.foreach(WRITERS_LIST_PATH).with_index do |writer, index|
-    if writer.include?(pattern); list_of_indicies.push(index)
+  def where(pattern)
+    file = File.open(@buffer, 'w')
+    list_of_indicies = []
+
+    File.foreach(@file).with_index do |writer, index|
+      if writer.include?(pattern); list_of_indicies.push(index)
+      end
     end
+
+    file.close
+    File.delete(@buffer) if File.exist?(@buffer)
+
+    list_of_indicies
   end
 
-  file.close
-  File.delete(BUFFER) if File.exist?(BUFFER)
-
-  list_of_indicies
-end
-
-def update(id, text)
-  file = File.open(BUFFER, 'w')
-  File.foreach(WRITERS_LIST_PATH).with_index do |writer, index|
-    file.puts(id == index ? text : writer)
-  end
-
-  file.close
-  File.write(WRITERS_LIST_PATH, File.read(BUFFER))
-
-  File.delete(BUFFER) if File.exist?(BUFFER)
-end
-
-def delete(id)
-  file = File.open(BUFFER, 'w')
-  File.foreach(WRITERS_LIST_PATH).with_index do |writer, index|
-    if id-1 != index; file.puts(writer)
+  def update(id, text)
+    file = File.open(@buffer, 'w')
+    File.foreach(@file).with_index do |writer, index|
+      file.puts(id == index ? text : writer)
     end
+
+    file.close
+    File.write(@file, File.read(@buffer))
+
+    File.delete(@buffer) if File.exist?(@buffer)
   end
 
-  file.close
-  File.write(WRITERS_LIST_PATH, File.read(BUFFER))
+  def delete(id)
+    file = File.open(@buffer, 'w')
+    File.foreach(@file).with_index do |writer, index|
+      if id-1 != index; file.puts(writer)
+      end
+    end
 
-  File.delete(BUFFER) if File.exist?(BUFFER)
-end
+    file.close
+    File.write(@file, File.read(@buffer))
 
-def create(name)
-  File.write(WRITERS_LIST_PATH, "\n#{ name }", mode: "a")
+    File.delete(@buffer) if File.exist?(@buffer)
+  end
+
+  def create(name)
+    File.write(@file, "\n#{ name }", mode: "a")
+  end
 end
