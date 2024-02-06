af79856b86f5bef8cb214ea62b62034e01746c37
sergio <khotsenkos1993k@gmail.com>
2024-02-06 18:35:18 +0300

    Chenged greeting program

--- homework_lecture_2/spec/check_numbers_spec.rb
+++ homework_lecture_2/spec/check_numbers_spec.rb
@@ -1,2 +1,35 @@
-# frozen_string_literal: true
+require '/home/sergio/RubyAcademy/homework_lecture_2/check_numbers.rb'
 
+RSpec.describe 'check two numbers' do
+  describe '#check_numbers' do
+    subject { check_numbers(first, second) }
+
+    let(:first) { 1 }
+    let(:second) { 2 }
+
+    context 'sums two numbers' do
+      it { is_expected.to eq(3) }
+    end
+
+    context 'returns first number if second number is 20' do
+      let(:second) { 20 }
+
+      it { is_expected.to eq(1) }
+    end
+    context 'returns second number if first number is 20' do
+      let(:first) { 20 }
+
+      it { is_expected.to eq(2) }
+    end
+
+    context 'returns Пасхалка if two numbers are 20' do
+      let(:first) { 20 }
+      let(:second) { 20 }
+      it { is_expected.to eq("Пасхалка") }
+    end
+    context 'returns a clear response if not two numbers was given' do
+      let(:first) { "John" }
+      it { is_expected.to eq("Неправильный ввод. Введите два числа.") }
+    end
+  end
+end
--- homework_lecture_2/spec/greeting_spec.rb
+++ homework_lecture_2/spec/greeting_spec.rb
@@ -1,4 +1,40 @@
 require 'rspec'
-require 'greeting'
+require '/home/sergio/RubyAcademy/homework_lecture_2/greeting.rb'
 
-RSpec.describe
+RSpec.describe "Test greeting with user I/O" do
+
+  describe '#greeting' do
+
+    subject { greeting }
+
+    context "For people younger than 18" do
+
+      let(:result) { /Пожалуйста, введите ваше имя:
+Пожалуйста, введите вашу фамилию:
+Пожалуйста, введите ваш возраст:
+Привет, Адам Сендлер! Тебе меньше 18 лет, но начать учиться программировать никогда не рано!/ }
+
+      it "prints to stdout the message for people younger than 18" do
+        allow_any_instance_of(Kernel)
+          .to receive(:gets)
+                .and_return("Адам", "Сендлер", "11")
+        expect { greeting }.to output(result).to_stdout
+      end
+    end
+
+    context "For people older than 18" do
+
+      let(:result) { /Пожалуйста, введите ваше имя:
+Пожалуйста, введите вашу фамилию:
+Пожалуйста, введите ваш возраст:
+Привет, Марк Цукерберг! Самое время заняться делом!/ }
+
+      it "prints to stdout the message for people younger than 18" do
+        allow_any_instance_of(Kernel)
+          .to receive(:gets)
+                .and_return("Марк", "Цукерберг", "44")
+        expect { greeting }.to output(result).to_stdout
+      end
+    end
+  end
+end
